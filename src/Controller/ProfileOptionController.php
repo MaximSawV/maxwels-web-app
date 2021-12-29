ec1d2194b20b67379870929c2449188a8de627d3
Maxim <msaweljew.arbeit.v@gmail.com>
2021-12-29 11:45:50 +0100

    Profile Options

--- src/Controller/ProfileOptionController.php
+++ src/Controller/ProfileOptionController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\ProfileOptionsRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,9 +12,14 @@ use Symfony\Component\Security\Core\Security;
 class ProfileOptionController extends AbstractController
 {
     #[Route('user/{user}/profile/option', name: 'profile_option')]
-    public function index(Security $security): Response
+    public function index(int $user, Security $security, ProfileOptionsRepository $optionsRepository, UserRepository $userRepository): Response
     {
-        
+        $profileOptions = $optionsRepository->findBy(['User' => $userRepository->find($user)]);
+        $publicContact = $profileOptions[0]->getShowStatus();
+        $showStatus = $profileOptions[0]->getShowStatus();
+        $hidden = $profileOptions[0]->getHidden();
+        $darkmode = $profileOptions[0]->getDarkmode();
+
         return $this->render('profile_option/index.html.twig', [
             'publicContact' => $publicContact,
             'showStatus' => $showStatus,
